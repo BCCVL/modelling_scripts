eb5cd349bd218375c8238bf7807c71a0a2d35e4a
erinmgraham <erin.graham@my.jcu.edu.au>
2013-09-11 17:06:35 +1000

    script to create html file of output

---
+++ create.html.R
@@ -0,0 +1,32 @@
+# this script will combine's R's output files into a single html doc
+
+install.packages(c("R2HTML", "png"))
+library(R2HTML) 
+library(png)
+
+# define the species
+species = "ABT"
+
+# define where the model outputs are stored
+wd = "/home/jc140298/sprint2/ABT/output_bioclim/"
+
+setwd(wd)
+
+# read in model outputs
+auccurve = readPNG(paste(wd, "AUC.png", sep=""))
+accuracystats = read.csv(paste(wd, "combined.modelEvaluation.csv", sep=""),	row.names=c(1))
+
+# create the output file 
+target <- HTMLInitFile(outdir=wd, filename=paste(species,"_output", sep=""), BackGroundColor="#CCCCCC")
+
+# add content
+HTML(paste("<center><br><H1>Model Output for ", species, sep=""), file=target)
+
+HTML("<br><H2>AUC:ROC curve", file=target)
+HTMLInsertGraph("AUC.png", file=target)
+
+HTML("<br><H2>Accuracy measures",file=target)
+HTML(accuracystats, file=target)
+
+# close the file
+HTMLEndFile()
